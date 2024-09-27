b2ece0f015a31ff3b7ec31075fd42593d48ef21f
nishmaha176 <nishmaha176@SDS-L-F81HC63>
2024-09-27 10:16:06 -0400

    Add project files.

---
+++ T Man's Galaxy Gas/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace T_Man_s_Galaxy_Gas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.galaxyGasLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mascotMaskLabel = new System.Windows.Forms.Label();
+            this.dispenserLabel = new System.Windows.Forms.Label();
+            this.galaxyGasInput = new System.Windows.Forms.TextBox();
+            this.maskInput = new System.Windows.Forms.TextBox();
+            this.dispenserInput = new System.Windows.Forms.TextBox();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.TextBox();
+            this.taxOutput = new System.Windows.Forms.TextBox();
+            this.totalOutput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.payButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(-2, -3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 472);
+            this.label2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(23, -3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(660, 20);
+            this.label3.TabIndex = 2;
+            // 
+            // galaxyGasLabel
+            // 
+            this.galaxyGasLabel.AutoSize = true;
+            this.galaxyGasLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.galaxyGasLabel.Location = new System.Drawing.Point(33, 41);
+            this.galaxyGasLabel.Name = "galaxyGasLabel";
+            this.galaxyGasLabel.Size = new System.Drawing.Size(154, 14);
+            this.galaxyGasLabel.TabIndex = 3;
+            this.galaxyGasLabel.Text = "Strawberry Galaxy Gas";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(671, -3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 472);
+            this.label1.TabIndex = 0;
+            // 
+            // mascotMaskLabel
+            // 
+            this.mascotMaskLabel.AutoSize = true;
+            this.mascotMaskLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.mascotMaskLabel.Location = new System.Drawing.Point(33, 73);
+            this.mascotMaskLabel.Name = "mascotMaskLabel";
+            this.mascotMaskLabel.Size = new System.Drawing.Size(84, 14);
+            this.mascotMaskLabel.TabIndex = 4;
+            this.mascotMaskLabel.Text = "Mascot Mask";
+            // 
+            // dispenserLabel
+            // 
+            this.dispenserLabel.AutoSize = true;
+            this.dispenserLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.dispenserLabel.Location = new System.Drawing.Point(33, 103);
+            this.dispenserLabel.Name = "dispenserLabel";
+            this.dispenserLabel.Size = new System.Drawing.Size(168, 14);
+            this.dispenserLabel.TabIndex = 5;
+            this.dispenserLabel.Text = "Whipped Cream Dispenser";
+            // 
+            // galaxyGasInput
+            // 
+            this.galaxyGasInput.Location = new System.Drawing.Point(207, 41);
+            this.galaxyGasInput.Name = "galaxyGasInput";
+            this.galaxyGasInput.Size = new System.Drawing.Size(100, 20);
+            this.galaxyGasInput.TabIndex = 6;
+            // 
+            // maskInput
+            // 
+            this.maskInput.Location = new System.Drawing.Point(207, 67);
+            this.maskInput.Name = "maskInput";
+            this.maskInput.Size = new System.Drawing.Size(100, 20);
+            this.maskInput.TabIndex = 7;
+            // 
+            // dispenserInput
+            // 
+            this.dispenserInput.Location = new System.Drawing.Point(207, 103);
+            this.dispenserInput.Name = "dispenserInput";
+            this.dispenserInput.Size = new System.Drawing.Size(100, 20);
+            this.dispenserInput.TabIndex = 8;
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.AutoSize = true;
+            this.taxLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.taxLabel.Location = new System.Drawing.Point(33, 272);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(77, 14);
+            this.taxLabel.TabIndex = 9;
+            this.taxLabel.Text = "Tax Amount";
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.AutoSize = true;
+            this.subtotalLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.subtotalLabel.Location = new System.Drawing.Point(33, 246);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(63, 14);
+            this.subtotalLabel.TabIndex = 10;
+            this.subtotalLabel.Text = "Subtotal";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.totalLabel.Location = new System.Drawing.Point(33, 299);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(42, 14);
+            this.totalLabel.TabIndex = 11;
+            this.totalLabel.Text = "Total";
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.Location = new System.Drawing.Point(116, 243);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(100, 20);
+            this.subtotalOutput.TabIndex = 12;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.Location = new System.Drawing.Point(116, 269);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(100, 20);
+            this.taxOutput.TabIndex = 13;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.Location = new System.Drawing.Point(116, 299);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(100, 20);
+            this.totalOutput.TabIndex = 14;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(78, 170);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(138, 36);
+            this.calculateButton.TabIndex = 15;
+            this.calculateButton.Text = "Calculate Price";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.AutoSize = true;
+            this.tenderedLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.tenderedLabel.Location = new System.Drawing.Point(33, 362);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(63, 14);
+            this.tenderedLabel.TabIndex = 16;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(101, 359);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(100, 20);
+            this.tenderedInput.TabIndex = 17;
+            // 
+            // payButton
+            // 
+            this.payButton.Location = new System.Drawing.Point(101, 385);
+            this.payButton.Name = "payButton";
+            this.payButton.Size = new System.Drawing.Size(100, 28);
+            this.payButton.TabIndex = 18;
+            this.payButton.Text = "Pay";
+            this.payButton.UseVisualStyleBackColor = true;
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.AutoSize = true;
+            this.changeLabel.Font = new System.Drawing.Font("Courier New", 8F);
+            this.changeLabel.Location = new System.Drawing.Point(33, 431);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(49, 14);
+            this.changeLabel.TabIndex = 19;
+            this.changeLabel.Text = "Change";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 425);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 464);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.payButton);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.dispenserInput);
+            this.Controls.Add(this.maskInput);
+            this.Controls.Add(this.galaxyGasInput);
+            this.Controls.Add(this.dispenserLabel);
+            this.Controls.Add(this.mascotMaskLabel);
+            this.Controls.Add(this.galaxyGasLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "T Man\'s Galaxy Gas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label galaxyGasLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label mascotMaskLabel;
+        private System.Windows.Forms.Label dispenserLabel;
+        private System.Windows.Forms.TextBox galaxyGasInput;
+        private System.Windows.Forms.TextBox maskInput;
+        private System.Windows.Forms.TextBox dispenserInput;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.TextBox subtotalOutput;
+        private System.Windows.Forms.TextBox taxOutput;
+        private System.Windows.Forms.TextBox totalOutput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button payButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ T Man's Galaxy Gas/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_Man_s_Galaxy_Gas
+{
+    public partial class Form1 : Form
+    {
+        double galaxyGas;
+        double mask;
+        double dispenser;
+        double subtotal;
+        double tax = 0.13;
+        double taxAmount;
+        double total;
+        double tendered;
+        double change;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        
+
+        
+    }
+}
